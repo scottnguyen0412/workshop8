d5d9c1271d73684be36db2636218d7d3af85de60
scottnguyen0412 <74314165+scottnguyen0412@users.noreply.github.com>
2021-06-18 16:40:46 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php 
+		
+	# DATABASE credential
+	$host = "localhost";
+	$db_name = "postgres";
+	$db_user = "postgres";
+	$db_pwd = "123qwe";
+	# Create connection to Heroku Postgres
+	$conn_string = "host=$host port=5432 dbname=$db_name user=$db_user password=$db_pwd";
+	
+?
