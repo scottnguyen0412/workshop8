0982639f5c5ccece906a39fe47c087c91855e7d0
scottnguyen0412 <74314165+scottnguyen0412@users.noreply.github.com>
2021-06-18 16:48:11 +0700

    Update config.php

--- config.php
+++ config.php
@@ -1,11 +1,11 @@
 <?php 
 		
 	# DATABASE credential
-	$host = "localhost";
-	$db_name = "postgres";
-	$db_user = "postgres";
-	$db_pwd = "123qwe";
+	$host = "ec2-50-17-255-120.compute-1.amazonaws.com";
+	$db_name = "d30gc3fkv4vald";
+	$db_user = "jltjkscsnqvcrg";
+	$db_pwd = "9a93c55fd08304c72c6f10db9255cae20134f2da994b05b77bbb55a91f152786";
 	# Create connection to Heroku Postgres
 	$conn_string = "host=$host port=5432 dbname=$db_name user=$db_user password=$db_pwd";
 	
-?
+?>
